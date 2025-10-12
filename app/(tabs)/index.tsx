5b185ea38ed83b7365c74bbea14feddc30e05d21
boodovskiy <dzak1987@mail.ru>
2025-10-12 13:50:21 +0300

    Conditionally render content for SignedIn / SignedOut

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,10 @@
 import { Text, View } from "@/components/Themed";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 
 export default function TabOneScreen() {
+  const { user } = useUser();
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="font-bold text-2xl text-red-500">Tab One</Text>
@@ -9,6 +13,19 @@ export default function TabOneScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        {/* <SignOutButton /> */}
+      </SignedIn>
+      <SignedOut>
+        <Link href="../(auth)">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="../(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
     </View>
   );
 }
