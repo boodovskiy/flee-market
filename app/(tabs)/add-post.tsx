b86ad14b5adad6e2c927b516091b195b17c2ee36
boodovskiy <dzak1987@mail.ru>
2025-10-20 09:10:09 +0300

    Install Formik and add Add Post Form

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,6 +1,13 @@
 import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { Formik } from "formik";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { app } from "../firebaseConfig";
 
 interface Category {
@@ -34,8 +41,62 @@ export default function AddPostScreen() {
   }, []);
 
   return (
-    <View>
-      <Text>AddPostScreen</Text>
+    <View className="p-10">
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+          category: "",
+          address: "",
+          price: "",
+          image: "",
+        }}
+        onSubmit={(value) => console.log(value)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Title"
+              value={values.title}
+              onChangeText={handleChange("title")}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Description"
+              value={values.description}
+              numberOfLines={5}
+              onChangeText={handleChange("description")}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Price"
+              value={values.price}
+              keyboardType="number-pad"
+              onChangeText={handleChange("price")}
+            />
+            {/* Category List Dropdown */}
+            <TouchableOpacity
+              onPress={() => handleSubmit()}
+              className="p-4 bg-blue-500 mt-7 rounded-lg"
+            >
+              <Text className="text-white text-center uppercase">Submit</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    paddingHorizontal: 15,
+    marginTop: 10,
+    marginBottom: 5,
+    fontSize: 16,
+  },
+});
