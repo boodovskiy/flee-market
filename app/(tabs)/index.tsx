431eb90a938e827138d7812c353972b9f24129e4
boodovskiy <dzak1987@mail.ru>
2025-10-30 10:04:26 +0200

    Add Slider component and fetch sliders from Firestore

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,22 @@
 import Header from "@/components/HomeScreen/Header";
+import Slider from "@/components/HomeScreen/Slider";
 import { View } from "@/components/Themed";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { useEffect } from "react";
+import { app } from "../firebaseConfig";
 
 export default function HomeScreen() {
+  const db = getFirestore(app);
+  const getSliders = async () => {
+    const querySnapshot = await getDocs(collection(db, "Sliders"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    });
+  };
+
+  useEffect(() => {
+    getSliders();
+  }, []);
   return (
     <View className="py-8 px-6 flex-1">
       <Header />
@@ -10,6 +25,7 @@ export default function HomeScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+      <Slider />
     </View>
   );
 }
