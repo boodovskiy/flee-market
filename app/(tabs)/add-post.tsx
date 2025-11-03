c6f907ec08937585baeb326a8a8623a85073bab1
boodovskiy <dzak1987@mail.ru>
2025-11-03 10:21:13 +0200

    Add KeyBoardAvoidingView for add post

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -9,6 +9,8 @@ import {
   ActivityIndicator,
   Alert,
   Image,
+  KeyboardAvoidingView,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
@@ -104,116 +106,120 @@ export default function AddPostScreen() {
   };
 
   return (
-    <View className="p-10">
-      <Text className="text-[27px] font-bold">Add New Post</Text>
-      <Text className="text-[16px] text-gray-500  mb-7">
-        Create New Post and Start Selling
-      </Text>
-      <Formik
-        initialValues={{
-          title: "",
-          description: "",
-          category: "",
-          address: "",
-          price: "",
-          image: "",
-          userName: "",
-          userEmail: "",
-          userImage: "",
-        }}
-        onSubmit={(value) => onSubmitMethod(value)}
-        validate={(values) => {
-          const errors: FormikErrors<FormValues> = {};
-          if (!values.title) {
-            console.log("Title is required");
-            ToastAndroid.show("Title is required", ToastAndroid.SHORT);
-            errors.title = "Title is required";
-          }
-          return errors;
-        }}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          setFieldValue,
-          errors,
-        }) => (
-          <View>
-            <TouchableOpacity onPress={pickImage}>
-              {image ? (
-                <Image
-                  source={{ uri: image }}
-                  style={{ width: 100, height: 100 }}
-                />
-              ) : (
-                <Image
-                  source={require("./../../assets/images/placeholder.jpg")}
-                  style={{ width: 100, height: 100, borderRadius: 15 }}
-                />
-              )}
-            </TouchableOpacity>
-            <TextInput
-              style={styles.input}
-              placeholder="Title"
-              value={values.title}
-              onChangeText={handleChange("title")}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Description"
-              value={values.description}
-              numberOfLines={5}
-              onChangeText={handleChange("description")}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Price"
-              value={values.price}
-              keyboardType="number-pad"
-              onChangeText={handleChange("price")}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Address"
-              value={values?.address}
-              onChangeText={handleChange("address")}
-            />
-            <View style={{ borderWidth: 1, borderRadius: 10, marginTop: 15 }}>
-              <Picker
-                selectedValue={values?.category}
-                onValueChange={(itemValue) =>
-                  setFieldValue("category", itemValue)
-                }
-                className="border-2"
+    <KeyboardAvoidingView>
+      <ScrollView className="p-10">
+        <Text className="text-[27px] font-bold">Add New Post</Text>
+        <Text className="text-[16px] text-gray-500  mb-7">
+          Create New Post and Start Selling
+        </Text>
+        <Formik
+          initialValues={{
+            title: "",
+            description: "",
+            category: "",
+            address: "",
+            price: "",
+            image: "",
+            userName: "",
+            userEmail: "",
+            userImage: "",
+          }}
+          onSubmit={(value) => onSubmitMethod(value)}
+          validate={(values) => {
+            const errors: FormikErrors<FormValues> = {};
+            if (!values.title) {
+              console.log("Title is required");
+              ToastAndroid.show("Title is required", ToastAndroid.SHORT);
+              errors.title = "Title is required";
+            }
+            return errors;
+          }}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            setFieldValue,
+            errors,
+          }) => (
+            <View>
+              <TouchableOpacity onPress={pickImage}>
+                {image ? (
+                  <Image
+                    source={{ uri: image }}
+                    style={{ width: 100, height: 100 }}
+                  />
+                ) : (
+                  <Image
+                    source={require("./../../assets/images/placeholder.jpg")}
+                    style={{ width: 100, height: 100, borderRadius: 15 }}
+                  />
+                )}
+              </TouchableOpacity>
+              <TextInput
+                style={styles.input}
+                placeholder="Title"
+                value={values.title}
+                onChangeText={handleChange("title")}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Description"
+                value={values.description}
+                numberOfLines={5}
+                onChangeText={handleChange("description")}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Price"
+                value={values.price}
+                keyboardType="number-pad"
+                onChangeText={handleChange("price")}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Address"
+                value={values?.address}
+                onChangeText={handleChange("address")}
+              />
+              <View style={{ borderWidth: 1, borderRadius: 10, marginTop: 15 }}>
+                <Picker
+                  selectedValue={values?.category}
+                  onValueChange={(itemValue) =>
+                    setFieldValue("category", itemValue)
+                  }
+                  className="border-2"
+                >
+                  {categoryList &&
+                    categoryList.map((item, index) => (
+                      <Picker.Item
+                        key={index}
+                        label={item.name}
+                        value={item.name}
+                      />
+                    ))}
+                </Picker>
+              </View>
+              <TouchableOpacity
+                onPress={() => handleSubmit()}
+                className="p-4 bg-blue-500 mt-10 rounded-lg"
+                style={{ backgroundColor: loading ? "#ccc" : "#007BFF" }}
+                disabled={loading}
               >
-                {categoryList &&
-                  categoryList.map((item, index) => (
-                    <Picker.Item
-                      key={index}
-                      label={item.name}
-                      value={item.name}
-                    />
-                  ))}
-              </Picker>
+                {loading ? (
+                  <ActivityIndicator color="#fff" />
+                ) : (
+                  <Text className="text-white text-center uppercase">
+                    Submit
+                  </Text>
+                )}
+              </TouchableOpacity>
             </View>
-            <TouchableOpacity
-              onPress={() => handleSubmit()}
-              className="p-4 bg-blue-500 mt-10 rounded-lg"
-              style={{ backgroundColor: loading ? "#ccc" : "#007BFF" }}
-              disabled={loading}
-            >
-              {loading ? (
-                <ActivityIndicator color="#fff" />
-              ) : (
-                <Text className="text-white text-center uppercase">Submit</Text>
-              )}
-            </TouchableOpacity>
-          </View>
-        )}
-      </Formik>
-    </View>
+          )}
+        </Formik>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
