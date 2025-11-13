0603f58ef53ecf624a124cad7f62a9fa1b87477d
boodovskiy <dzak1987@mail.ru>
2025-11-13 11:51:15 +0200

    Fix routing

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function HomeStackLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="home" />
-      <Stack.Screen name="(category-details)/[categoryName]" />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(category-details)" />
       <Stack.Screen
         name="product-details/[id]"
         options={{
