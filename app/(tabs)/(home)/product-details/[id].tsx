639e128e91c8d308771f2912ac22f53684e83519
boodovskiy <dzak1987@mail.ru>
2025-11-15 12:58:22 +0200

    Parse local search params and display product data

--- app/(tabs)/(home)/product-details/[id].tsx
+++ app/(tabs)/(home)/product-details/[id].tsx
@@ -1,16 +1,40 @@
 import { PostItemType } from "@/types";
 import { useLocalSearchParams } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function ProductDetail() {
   const { item } = useLocalSearchParams();
-  const [product, setProduct] = useState<PostItemType[]>([]);
+  const [product, setProduct] = useState<PostItemType | null>(null);
 
   useEffect(() => {
-    console.log(item);
-    //setProduct();
-  }, [item]);
+    console.log("hello:" + item);
+    if (item) {
+      try {
+        const parsedItem: PostItemType = JSON.parse(item as string);
+        setProduct(parsedItem);
+      } catch (error) {
+        console.error("Failed to parse item:", error);
+      }
+    }
+  }, []);
 
-  return <View style={{ flex: 1 }}>{/* <Image /> */}</View>;
+  return (
+    <View style={{ flex: 1 }}>
+      <Image source={{ uri: product?.image }} className="h-[350px] w-full" />
+
+      <View className="p-3">
+        <Text className="text-[24px] font-bold">{product?.title}</Text>
+        <View className="items-baseline">
+          <Text className="p-1 px-2 rounded-full bg-blue-200 text-blue-500">
+            {product?.category}
+          </Text>
+        </View>
+        <Text className="mt-3 font-bold text-[20px]">Description</Text>
+        <Text className="text-[17px] text-gray-500">
+          {product?.description}
+        </Text>
+      </View>
+    </View>
+  );
 }
